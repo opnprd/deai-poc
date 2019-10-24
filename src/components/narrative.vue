54e69f4b05f19839231a23923ea8ae5364612e80
Giles Dring <gilesdring@gmail.com>
2019-10-24 09:31:18 +0100

    Add narrative section to report

---
+++ src/components/narrative.vue
@@ -0,0 +1,14 @@
+<script>
+import marked from 'marked';
+export default {
+  props: [ 'markdown' ],
+  computed: {
+    html() {
+      return marked(this.markdown);
+    }
+  }
+}
+</script>
+<template>
+  <section v-html="html" />
+</template>
--- src/components/section.vue
+++ src/components/section.vue
@@ -1,10 +1,12 @@
 <script>
 import map from './map.vue';
+import narrative from './narrative.vue';
 
 export default {
   props: [ 'title', 'content' ],
   components: {
     'data-map': map,
+    'report-narrative': narrative,
   }
 }
 </script>
@@ -13,6 +15,7 @@ export default {
     <h1>{{ title }}</h1>
     <div v-for='(block, index) in content' :key='index'>
       <data-map v-if='block.type === "map"' :view='block.view' />
+      <report-narrative v-if='block.type === "narrative"' v-bind="block" />
     </div>
   </section>
 </template>
